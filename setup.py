0a98800ccb10625cbd3d35d8c4dd39260a403828
larrykirschner <larrykirschner@gmail.com>
2018-11-16 07:44:12 -0800

    python3.7

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     setup_requires   = setup_requirements,
     extras_require   = dev_requirements,
     tests_require    = test_requirements,
-    python_requires  = '>=3.6',
+    python_requires  = '>=3.7',
 
     test_suite = 'tests',
 
