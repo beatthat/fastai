16e36bfc1ea4860b98591fa0ea5862da8d378404
larrykirschner <larrykirschner@gmail.com>
2018-11-27 23:23:50 -0800

    sep docker run scripts for fastai 0.7 1.0

---
+++ run-docker-0.7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+ROOT=`pwd`
+
+NAME=fastai
+
+mkdir -p docker_host
+HOST_MOUNT=`pwd`/docker_host
+
+docker run \
+  --name ${NAME} \
+  -it \
+  --rm \
+  --shm-size 16G \
+  --runtime=nvidia \
+  -p 8888:8888 \
+  --mount type=bind,src=${HOST_MOUNT},target=/notebooks/docker_host \
+  --workdir /notebooks \
+  fastai:0.7
+
+
+# docker exec -it ${NAME} bash
